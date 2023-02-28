e0fbe45a6e5d291ac5cfaf04c1cc70f55767e15b
thatshubham <shubhambose@protonmail.com>
2023-02-28 20:09:51 +0530

    fixed svg issues

--- app/Navbar.js
+++ app/Navbar.js
@@ -1,4 +1,3 @@
-
 import Link from "next/link"
 
 export default function Navbar() {
@@ -11,11 +10,7 @@ export default function Navbar() {
                 <Link className="text-4xl font-bold text-gray-700" href="/">Find Movies</Link>
             </nav>
             <Link href="https://github.com/thatshubham">
-            <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0 text-gray-100">Github
-            <svg fill="black" stroke="gray" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-            </button>
+            <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0 text-gray-100">Github</button>
             </Link>
         </div>
     </header>
