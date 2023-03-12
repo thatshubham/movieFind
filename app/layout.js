5c363fe98d296d356086889b181db5abc2f2e75e
thatshubham <shubhambose@protonmail.com>
2023-03-12 23:02:17 +0530

    vercel package

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Movie Finder',
@@ -11,9 +12,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="flex items-center flex-col justify-between min-h-screen mx-auto">
-      <Navbar />
-      {children}
-      <Footer />
+        <Navbar />
+        {children}
+        <Footer />
+        <Analytics />
       </body>
     </html>
   )
